24ed6a444be7ee5fda7acaec39ee378961524d21
Tim Case <tim@2drops.net>
2017-02-14 10:26:37 +0700

    Add user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # include ErrorSerializer
-  # skip_before_action :authenticate, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
   def index
     render json: User.all
   end
@@ -12,16 +12,17 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {}, status: 200
+      render json: {jwt: JWTWrapper.encode(user)}.to_json, status: :created
     else
-      render json: ErrorSerializer.serialize(user.errors), status: 422
+      render json: {error: 'oops'}, status: :unprocessable_entity
+      # render json: ErrorSerializer.serialize(user.errors), status: 422
     end
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   post 'user_token' => 'tokens#create'
   resources :todos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
